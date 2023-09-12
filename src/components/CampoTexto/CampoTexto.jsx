0f5d037228c1e20174d0db41bc7ee52933f04b0d
AdrianL824 <adrian.lopez.or2408@gmail.com>
2023-09-12 11:04:20 -0400

    Clase 6 React and Js add: Control input

--- src/components/CampoTexto/CampoTexto.jsx
+++ src/components/CampoTexto/CampoTexto.jsx
@@ -1,13 +1,21 @@
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  const placeholderDots = `${props.placeholder}...`;
+
+  const manejarCambio = (e) => {
+    props.setValor(e.target.value);
+  };
+
   return (
     <div className="campo-texto">
       <label htmlFor="">{props.titulo}</label>
       <input
         type="text"
-        placeholder={props.placeholder}
+        placeholder={placeholderDots}
         required={props.required}
+        value={props.valor}
+        onChange={manejarCambio}
       />
     </div>
   );
--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto";
 import ListaOpciones from "../ListaOpciones/ListaOpciones";
 import Boton from "../Boton/Boton";
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState("");
+  const [puesto, setPuesto] = useState("");
+  const [foto, setFoto] = useState("");
+
   const manejarEnvio = (e) => {
     e.preventDefault();
+    console.log("Enviando");
+    let datos = {
+      nombre: nombre,
+      puesto: puesto,
+      foto: foto,
+    };
+    console.log(datos);
   };
 
   return (
     <section className="formulario">
       <form onSubmit={manejarEnvio}>
         <h2>Rellena el formulario para crear el colaborador.</h2>
-        <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required />
-        <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required />
+        <CampoTexto
+          titulo="Nombre"
+          placeholder="Ingresar nombre"
+          required
+          valor={nombre}
+          setValor={setNombre}
+        />
+        <CampoTexto
+          titulo="Puesto"
+          placeholder="Ingresar puesto"
+          required
+          valor={puesto}
+          setValor={setPuesto}
+        />
         <CampoTexto
           titulo="Foto"
           placeholder="Ingresar enlace de foto"
           required
+          valor={foto}
+          setValor={setFoto}
         />
         <ListaOpciones />
         <Boton>Crear</Boton>
