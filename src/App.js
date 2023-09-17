43873fc168d147a867c8b1cb91cb0da440603b28
AdrianL824 <adrian.lopez.or2408@gmail.com>
2023-09-16 20:14:56 -0400

    Clase8 React and JS add:Colaborador

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ import Equipo from "./components/Equipo/Equipo";
 
 function App() {
   const [mostrarFormulario, actualizarMostrar] = useState(false);
+  const [colaboradores, actualizarColaboradores] = useState([]);
+
   const cambiarMostrar = () => {
     actualizarMostrar(!mostrarFormulario);
   };
-  // Ternario --> condition ? seMuestra : noSeMuestra
-  // Otro metodo --> Condicion && seMuestra
+  // Ternario --> condition ? seMuestra : noSeMuestra --> Metodo para desaparecer y aparecer componentes
+  // Otro metodo --> condicion && seMuestra --> Metodo para desaparecer y aparecer componentes
 
+  const registrarColaborador = (colaborador) => {
+    console.log("nuevo colaboradr", colaborador);
+    //Spread operator
+    actualizarColaboradores([...colaboradores, colaborador]);
+  };
   //Lista de equipos
   const equipos = [
     {
@@ -49,14 +56,21 @@ function App() {
       {/* <Header></Header> 
       {}<Header()  */}
       <Header />
-      {mostrarFormulario ? (
-        <Formulario equipos={equipos.map((equipo) => equipo.titulo)} />
-      ) : (
-        <></>
+      {mostrarFormulario && (
+        <Formulario
+          equipos={equipos.map((equipo) => equipo.titulo)}
+          registrarColaborador={registrarColaborador}
+        />
       )}
       <MiOrg cambiarMostrar={cambiarMostrar} />
       {equipos.map((equipo) => (
-        <Equipo datos={equipo} key={equipo.titulo} />
+        <Equipo
+          datos={equipo}
+          key={equipo.titulo}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.equipo === equipo.titulo
+          )}
+        />
       ))}
     </div>
   );
--- src/components/Equipo/Equipo.jsx
+++ src/components/Equipo/Equipo.jsx
@@ -4,20 +4,23 @@ import "./Equipo.css";
 const Equipo = (props) => {
   //Destructuracion
   const { colorPrimario, colorSecundario, titulo } = props.datos;
+  const { colaboradores } = props;
 
   const secundario = { backgroundColor: colorSecundario };
   const primario = { borderColor: colorPrimario };
+  //console.log(colaboradores.length > 0);
 
   return (
-    <section className="equipo" style={secundario}>
-      <h3 style={primario}>{titulo}</h3>
-      <div className="colaboradores">
-        <Colaborador />
-        <Colaborador />
-        <Colaborador />
-        <Colaborador />
-      </div>
-    </section>
+    colaboradores.length > 0 && (
+      <section className="equipo" style={secundario}>
+        <h3 style={primario}>{titulo}</h3>
+        <div className="colaboradores">
+          {colaboradores.map((colaborador, index) => (
+            <Colaborador datos={colaborador} key={index} />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 export default Equipo;
